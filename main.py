27ee28d7311c627a7905b1810cca5423c1b464ff
Mateus C. <100164090+m4ttpizz4@users.noreply.github.com>
2024-10-31 23:29:02 -0300

    Atualizações importantes!
    # Atualizações:
    - Modo escuro adicionado;
    - Só isso mesmo KADKSKDSKADSKDASK;

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+#bibliotecas:
 from tkinter import *
 import tkinter.messagebox
 
-#função para atualizar a cor da tela:
+#função para atualizar cor da rolagem:
 def atualizar_cor(*args):
     r = s_red.get()
     g = s_green.get()
@@ -9,8 +10,41 @@ def atualizar_cor(*args):
     cor = f'#{r:02x}{g:02x}{b:02x}'
     tela.configure(bg=cor)
 
-preto = "#444466"
+#função para alternar a cor do tema:
+def toggle_dark_mode():
+    global dark_mode
+    if dark_mode:
+        janela.configure(bg=preto)
+        tela.configure(bg=preto)
+        frame_direita.configure(bg=preto)
+        frame_baixo.configure(bg=preto)
+        l_red.configure(bg=preto)
+        l_green.configure(bg=preto)
+        l_blue.configure(bg=preto)
+        s_red.configure(bg=preto)
+        s_green.configure(bg=preto)
+        s_blue.configure(bg=preto)
+        dark_mode_button.configure(text="Dark Mode")
+        dark_mode = False
+    else:
+        janela.configure(bg=preto)
+        tela.configure(bg=branco)
+        frame_direita.configure(bg=preto)
+        frame_baixo.configure(bg=preto)
+        l_red.configure(bg=preto)
+        l_green.configure(bg=preto)
+        l_blue.configure(bg=preto)
+        s_red.configure(bg=preto)
+        s_green.configure(bg=preto)
+        s_blue.configure(bg=preto)
+        dark_mode_button.configure(text="Light Mode")
+        dark_mode = True
+
+#cores:
+preto = "#181818"
 branco = "#feffff"
+dark_mode = False
+
 #criando interface:
 janela = Tk()
 janela.geometry('530x250')
@@ -20,11 +54,11 @@ janela.configure(bg=branco)
 tela = Label(janela, bg=preto, width=40, height=10, bd=1)
 tela.grid(row=0, column=0, columnspan=3)
 
-#frame direita:
+#freme direito:
 frame_direita = Frame(janela, bg=branco)
 frame_direita.grid(row=1, column=0)
 
-#frame baixo:
+#freme baixo:
 frame_baixo = Frame(janela, bg=branco)
 frame_baixo.grid(row=2, column=0, columnspan=3, pady=15)
 
@@ -46,5 +80,9 @@ l_blue.grid(row=2, column=0)
 s_blue = Scale(frame_direita, from_=0, to=255, length=150, bg=branco, fg="blue", orient=HORIZONTAL, command=atualizar_cor)
 s_blue.grid(row=2, column=1)
 
-#iniciando o loop principal:
+#botão do modo escuro brabo:
+dark_mode_button = Button(janela, text="Modo escuro", command=toggle_dark_mode)
+dark_mode_button.grid(row=3, column=0, columnspan=3, pady=10)
+
+#loop da janela:
 janela.mainloop()
